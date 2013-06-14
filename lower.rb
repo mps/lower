b2afc0d3687e59531f6c41fa29ef8bac870a3347
Matthew Strickland <matthew@idlefusion.com>
2013-06-14 12:43:15 -0300

    Update lower.rb

--- lower.rb
+++ lower.rb
@@ -3,8 +3,9 @@
 # Public: Renames all files in a given directory to make sure all words are lower cased.
 # Matthew Strickland
 #
-#
 # The first argument is the directory otherwise it defaults to the directory the script is located in.
+#
+# example usage: ruby lower.rb ~/my/cool/directory/path
 directory = ARGV[0]
 
 if directory.nil?
